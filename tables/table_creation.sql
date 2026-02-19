85e34fdadf0421c171e9abc1f943a64351123b36
Apurba Mukherjee <2005apurba@gmail.com>
2026-02-19 18:15:37 +0530

    edited table data

--- tables/table_creation.sql
+++ tables/table_creation.sql
@@ -1,19 +1,35 @@
 CREATE TABLE symbol (
-    s_id SERIAL,
-    symbol_id INT PRIMARY KEY,
-    company_name VARCHAR(100),
-    company_symbol VARCHAR(100),
-    sector VARCHAR(100),
-    created_on DATE
+    symbol_id SERIAL PRIMARY KEY,
+    company_name VARCHAR(100) NOT NULL,
+    company_symbol VARCHAR(20) UNIQUE NOT NULL,
+    sector VARCHAR(100) NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO symbol (company_name, company_symbol, sector)
+VALUES
+('Reliance Industries Ltd.', 'RELIANCE.NS', 'Energy'),
+('Tata Consultancy Services Ltd.', 'TCS.NS', 'Technology');
 
 
 SELECT * FROM symbol;
 
--- DROP TABLE SYMBOL;
+DROP TABLE SYMBOL;
 
 -- CREATE DATABASE stock_db;
 
 -- SELECT current_database();
 
 
+CREATE TABLE fundamentals (
+    fundamental_id SERIAL PRIMARY KEY,
+    symbol_id INT NOT NULL,
+    pe FLOAT NOT NULL,
+    peg FLOAT NOT NULL,
+    promoter_holding DECIMAL(5,2) NOT NULL,
+    created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (symbol_id)
+        REFERENCES symbol(symbol_id)
+        ON DELETE CASCADE
+);
