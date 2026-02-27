8360a45f128371b8159b9679908bd2cae7591fc2
Apurba Mukherjee <2005apurba@gmail.com>
2026-02-27 23:17:35 +0530

    edited

---
+++ backend/main.py
@@ -0,0 +1,3 @@
+import yfinance as yf
+import os
+import openai
---
+++ frontend/app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import requests
+import logging as log
+
+
+log.basicConfig(
+    level=log.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+BACKEND_URL = "http://localhost:8000/query"
+
+if "loading" not in st.session_state:
+    st.session_state.loading = False
+    
+
+
+st.title("AI Powered Stock Screener")
+
+nl_query = st.text_input("Enter your prompt here")
+
+submit_btn = st.button(
+    "Submit",
+    disabled=st.session_state.loading
+)
+
+if submit_btn:
+
+    if nl_query.strip() == "":
+        st.warning("Please enter a query.")
+    else:
+        st.session_state.loading = True
+
+        with st.spinner("Processing your query... Please wait"):
+            try:
+                response = requests.post(
+                    BACKEND_URL,
+                    params={"nl_query": nl_query}
+                )
+
+                if response.status_code == 200:
+                    data = response.json()
+                    log.info("Generated Successfully")
+                    st.json(data["dsl"])
+                    log.info("Query processed successfully")
+                else:
+                    log.error(f"HTTP Error {response.status_code}: {response.text}")
+                    #st.error(f"Request failed with status code {response.status_code}")
+
+            except Exception:
+                log.info("Backend connection failed")
+
+        st.session_state.loading = False
