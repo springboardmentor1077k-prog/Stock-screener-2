ddde07a2d631ef4c67df43ecf7733765abaf52f6
Dimple04152 <141268457+Dimple04152@users.noreply.github.com>
2026-03-05 15:40:08 +0530

    updated database.py

--- database.py
+++ database.py
@@ -1,5 +1,15 @@
+import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/stock_screener"
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
