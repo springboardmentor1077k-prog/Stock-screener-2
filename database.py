82d6a8b7f4f39b07f144e31109ee9480f4c83322
Varshitha152004 <guddetivarshitha4@gmail.com>
2026-04-04 12:18:15 +0530

    final

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
-import os
-DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASE_URL = "postgresql://postgres:newpassword123@localhost:5432/stock_screener"
 engine = create_engine(DATABASE_URL)
