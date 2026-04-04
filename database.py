6bf8053f7ce5f4c33b4280aff2788a8073db3e0f
Varshitha152004 <guddetivarshitha4@gmail.com>
2026-04-04 10:43:38 +0530

    fixed database for deployment

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine
-
-DATABASE_URL = "postgresql://postgres:newpassword123@localhost:5432/stock_screener"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
